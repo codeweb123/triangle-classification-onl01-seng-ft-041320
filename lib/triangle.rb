6f876606782b7af49ff7633a7b47b1a67f1427e6
Jiyon FL <jiyonj@hotmail.com>
2020-04-30 21:36:03 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,6 +1,22 @@
 class Triangle
-
+  attr_accessor :side1, :side2, :side3
+  @sides = []
+  
   def initialize(side1, side2, side3)
-
+    @side1, @side2, @side3 = side1, side2, side3
+    @sides = [side1, side2, side3]
   end
+
+  def kind
+    if(negative? == true || valid? == false)
+      raise TriangleError
+    
+
+
+
+
+
+
+
+  
 end
